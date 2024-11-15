4f52522ed310c0b442bbd0d31dee4fad01b9dbd5
AnketKohak <anketkohak40@gmail.com>
2024-11-15 13:57:37 +0530

    Initial Commit

---
+++ FireBase Learning/FireBase_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBase_LearningApp.swift
+//  FireBase Learning
+//
+//  Created by anket kohak on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FireBase_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
