5996a0553bcc87ff834faa97840008a6ebd59110
AnketKohak <anketkohak40@gmail.com>
2025-01-07 12:44:02 +0530

    New Screen added ProfileView here use enviromnetObject insetad of StateObject

---
+++ FireBase Learning/Screens/Auth/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  FireBase Learning
+//
+//  Created by Anket Kohak on 07/01/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
