211bf880cc8913c86629289413ee7f9565c6fc27
AnketKohak <anketkohak40@gmail.com>
2025-01-08 14:24:12 +0530

    forgot Password functionality added

---
+++ FireBase Learning/Screens/Auth/Views/EmailSentView.swift
@@ -0,0 +1,18 @@
+//
+//  EmailSentView.swift
+//  FireBase Learning
+//
+//  Created by Anket Kohak on 08/01/25.
+//
+
+import SwiftUI
+
+struct EmailSentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmailSentView()
+}
